318fc270d6bc5fd1e4345e59987e9ec494b26a94
Rafinal Haryokusumo Taloputra <89496855+RafinalHT@users.noreply.github.com>
2022-09-14 23:26:37 +0700

    Update urls.py

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,3 +1,4 @@
+from wishlist.views import show_xml #sesuaikan dengan nama fungsi yang dibuat
 from django.urls import path
 from wishlist.views import show_wishlist
 
@@ -5,4 +6,5 @@ app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_xml, name=’show_xml’),
 ]
